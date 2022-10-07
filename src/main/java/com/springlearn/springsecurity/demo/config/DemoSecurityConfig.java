6c6b10b18ae07aa8e82664968315beaa7095abf3
MdSaifulIslam <mohammadsaifulcsepstu@gmail.com>
2022-10-07 09:25:48 +0600

    custom login form init

--- src/main/java/com/springlearn/springsecurity/demo/config/DemoSecurityConfig.java
+++ src/main/java/com/springlearn/springsecurity/demo/config/DemoSecurityConfig.java
@@ -2,6 +2,7 @@ package com.springlearn.springsecurity.demo.config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.userdetails.User;
@@ -15,11 +16,17 @@ public class DemoSecurityConfig extends WebSecurityConfigurerAdapter {
 	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
 
 		UserBuilder users = User.withDefaultPasswordEncoder();
-		
-		auth.inMemoryAuthentication()
-		.withUser(users.username("SAIFUL").password("123").roles("ADMIN"))
-		.withUser(users.username("SALAM").password("123").roles("MANAGER"))
-		.withUser(users.username("SAKIB").password("123").roles("EMPLOYEE"));
+
+		auth.inMemoryAuthentication().withUser(users.username("SAIFUL").password("123").roles("ADMIN"))
+				.withUser(users.username("SALAM").password("123").roles("MANAGER"))
+				.withUser(users.username("SAKIB").password("123").roles("EMPLOYEE"));
+	}
+
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+
+		http.authorizeRequests().anyRequest().authenticated().and().formLogin().loginPage("/showLoginPage")
+				.loginProcessingUrl("/authenticateTheUser").permitAll();
 	}
 
 }
